b9241c4f6aad95adfb898baaba4df2d645838c41
Taarun Sridhar <84679836+taarunsridhar@users.noreply.github.com>
2023-02-19 21:20:35 +0530

    Update PES1UG20CS456.cpp

--- PES1UG20CS456.cpp
+++ PES1UG20CS456.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Test cpp file:\nfrom Taarun Sridhar, PES1UG20CS456";
+  cout << "Test cpp file: from Taarun Sridhar, PES1UG20CS456";
   return 0;
 }
